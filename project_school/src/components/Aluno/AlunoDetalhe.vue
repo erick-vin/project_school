8d9b5f02f08ecc145b27dd5a1908153701d0b124
erick.santos3 <erick.santos@segfy.com>
2022-04-28 16:42:36 -0300

    melhora aluno detalhe

--- project_school/src/components/Aluno/AlunoDetalhe.vue
+++ project_school/src/components/Aluno/AlunoDetalhe.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
     <TituloShare :texto="`Aluno: ${aluno.nome}`" />
+    <table>
+        <tbody>
+            <tr>
+                <td class="colPequeno">Matrícula:</td>
+                <td>
+                    <label for="">{{aluno.id}}</label>
+                    <input v-model="aluno.id" type="text"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="colPequeno">Nome:</td>
+                <td>
+                    <label for="">{{aluno.nome}}</label>
+                    <input v-model="aluno.nome" type="text"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="colPequeno">Sobrenome:</td>
+                <td>
+                    <label for="">{{aluno.sobrenome}}</label>
+                    <input v-model="aluno.sobrenome" type="text"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="colPequeno">Data Nascimento:</td>
+                <td>
+                    <label for="">{{aluno.dataNasc}}</label>
+                    <input v-model="aluno.dataNasc" type="text"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="colPequeno">Professor:</td>
+                <td>
+                    <label for="">{{aluno.professor.nome}}</label>
+                </td>
+            </tr>
+        </tbody>
+    </table>
   </div>
 </template>
 
@@ -13,7 +51,14 @@ export default {
   data() {
     return {
       aluno: {},
+      idAluno: this.$route.params.id
     };
+  },
+  created() {
+      this.$http
+        .get("http://localhost:3000/alunos/" + this.idAluno)
+        .then((res) => res.json())
+        .then((aluno) => (this.aluno = aluno));
   },
   methods: {
       
